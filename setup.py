12db98294e8ff75ea4de27de6b39db20380052a2
Anowarul Karim <anowarulkarim8@gmail.com>
2024-12-02 23:11:16 +0600

    third

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="dstructslib",  # New name without underscores or dashes
-    version="1.0.1",       # Start fresh or increment version if necessary
+    version="1.0.2",       # Start fresh or increment version if necessary
     install_requires=[],   # Add dependencies if any
     description="Data Structures Collections: BST and Linked Lists.",
     long_description=open("README.md").read(),
+    packages=find_packages(),
     long_description_content_type="text/markdown",
     author='Bhuyan Md Anowarul Kairm',                # Replace with your name
     author_email='anowarulkarim8@gmail.com', # Replace with your email
